dc97ab93aced1779107ce3497b8c0d8d5fb15d55
David_Díaz <diazhoilluisdavid@gmail.com>
2023-08-04 11:59:02 +0200

    css

--- pop.js
+++ pop.js
@@ -20,7 +20,7 @@ var btnAbrirPopup = document.getElementById('bet2'),
     let nomb,asunto, num;
     nomb=document.getElementById('obj1').value;
     asunto=document.getElementById('obj2').value;
-    num=9871121687;
+    num=6441116541;
   
     var win= window.open(`https://wa.me/${num}?text=Hola%20mi%20nombre%20es%20${nomb}%20me%20pueden%20ayudar%20con%20esto:%20${asunto}`,'_blank');
   }
